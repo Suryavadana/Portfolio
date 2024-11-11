db62f162ee28ca7ad4bb3321a3f2a934f0868e62
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-11-11 09:38:35 -0600

    added About me

---
+++ Portfolio/src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/About.css';
+
+const About = () => {
+  return (
+    <section id='about' className='about'>
+        <h2>About Me</h2>
+            <p>
+                I'm a Passionate web developer with experience in building modern web applications
+                using React, Node.js , and more.
+            </p> 
+            <p>Tech stack: React, JavaScript, HTML, CSS, Node.js, Springboot, MySQL, Java</p>   
+    </section>
+  );
+}
+
+export default About;
